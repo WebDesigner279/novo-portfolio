2f82156b9fa50ffc89ef66ddb983da03e8e689a4
Valter Vieira Gomes Junior <tekraseg02@gmail.com>
2025-02-25 23:34:59 -0300

    configuração do next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true, // Ativa o App Router se estiver usando a estrutura do Next.js 14+
+    },
+  };
+  
+  module.exports = nextConfig;
+  
